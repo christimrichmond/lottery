92eae77e610346d3a5812ed86eed89c2e93c269d
christimrichmond <christimrichmond@gmail.com>
2018-01-26 11:55:17 -0500

    functional - need to work on front end

--- app.rb
+++ app.rb
@@ -1,18 +1,27 @@
 require "sinatra"
-require_relative "lottery.rb"
-
+require_relative "lottery_orig.rb"
 get '/' do
 	erb :index
 end
 
 post '/input' do
-	puts input
-	redirect 'results?'
+	num1 = params[:num1]
+	num2 = params[:num2]
+	num3 = params[:num3]
+	num4 = params[:num4]
+	num5 = params[:num5]
+	num6 = params[:num6]
+	redirect 'results?num1=' + num1 + '&num2=' + num2 + '&num3=' + num3 + '&num4=' + num4 + '&num5=' + num5 + '&num6=' + num6
 end
 
 get '/results' do
-	puts matches
-	erb :results
+	num1 = params[:num1]
+	num2 = params[:num2]
+	num3 = params[:num3]
+	num4 = params[:num4]
+	num5 = params[:num5]
+	num6 = params[:num6]
+	erb :results, :locals => {:num1 => num1, :num2 => num2, :num3 => num3, :num4 => num4, :num5 => num5, :num6 => num6}
 end
 
 	
--- lottery_orig.rb
+++ lottery_orig.rb
@@ -1,29 +1,12 @@
-puts "\nPlease enter 6 numbers, separated by a space:"
-input = gets.chomp(",").to_i #the integers input by user 
-lotto = 6.times.map{Random.rand(99)+1} #creates new array of the random 6 lotto numbers, 1 to 99
-matches = [lotto] & [input] #creates an intersection of the 2 sets of numbers, finding matching numbers, if any.
-num = matches.length #the number of matched numbers
+# puts "\nPlease enter 6 numbers, separated by a space:"
+# input = gets.chomp(",").to_i #the integers input by user 
+# lotto = 6.times.map{Random.rand(99)+1} #creates new array of the random 6 lotto numbers, 1 to 99
+# matches = [lotto] & [input] #creates an intersection of the 2 sets of numbers, finding matching numbers, if any.
+# num = matches.length #the number of matched numbers
 
-if input = lotto
-	puts "Congratulations! You have matched all numbers! You win a gazillion dollars!!\n\n
-The winning numbers are #{lotto}.\n\n"
-	elsif num == 5
-		puts "\nYou have matched 5 numbers! You win $1,000\n\n
-The winning numbers are #{lotto}.\n\n"	
-	elsif num == 4
-		puts "\nYou have matched 4 lotto numbers! You win $500\n\n
-The winning numbers are #{lotto}.\n\n"	
-	elsif num == 3
-		puts "\nYou have matched 3 lotto numbers! You win $100\n\n
-The winning numbers are #{lotto}.\n\n"
-	elsif num == 2
-		puts "\nYou have matched 2 lotto numbers! You win $50\n\n
-The winning numbers are #{lotto}.\n\n"	
-	elsif num == 1
-		puts "\nYou have only matched 1 lotto number. You are not a winner today.\n\n
-The winning numbers are #{lotto}.\n\n"
-	else
-		puts "Sorry, the winning numbers are #{lotto}.\n
-You have not matched any numbers this time. Please play again! \n\n"
+def winner(input,lotto)
+	matches = lotto & input
+	$num = matches.length
+	$num.to_i
 
 end
