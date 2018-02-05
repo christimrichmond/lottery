a9698aef1328e5f031549dd18c36aa42f675aca4
christimrichmond <christimrichmond@gmail.com>
2018-02-04 20:41:51 -0500

    heroku addon

--- lottery_test.rb
+++ lottery_test.rb
@@ -1,13 +1,15 @@
 require "minitest/autorun"
-require_relative "lottery.rb"
-Class LotteryProgramTest <Minitest::Test
+require_relative "lottery_orig.rb"
+Class Testlottery_orig <Minitest::Test
 
   def test_add_one_plus_one_equals_2
   	result = add(1,1)
   	assert_equal(2, result)
   end
 
-
+  def winner_as_input_number_matches_lotto_number
+  	assert_equal(lotto, input) 
+  end
 
 
 
